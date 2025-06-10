38c9af7cbe2c3ee61263588fdcea016245d02307
Sebastian Alvarez <ca.sebastianlv@gmail.com>
2025-06-09 22:56:23 -0300

    Limpiar archivos que no se usan

--- backend/src/main/java/py/com/progweb/parcial/rest/ApplicationConfig.java
+++
@@ -1,8 +0,0 @@
-package py.com.progweb.parcial.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class ApplicationConfig extends Application {
-}
